d9ba44793826b08ce4de765c19e1996abec6ba86
dorayo <yomingyo@gmail.com>
2014-07-18 14:50:46 +0800

    Initial Commit

---
+++ 倾音乐2.0/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  倾音乐2.0
+//
+//  Created by qingyun on 14-7-18.
+//  Copyright (c) 2014年 MJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 倾音乐2.0/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  倾音乐2.0
+//
+//  Created by qingyun on 14-7-18.
+//  Copyright (c) 2014年 MJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
